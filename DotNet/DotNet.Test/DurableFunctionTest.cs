611c437984d4e2e9a773b409c71bdfb6227429c3
Adrian Suhov <adrian.suhov@gilead.com>
2021-09-09 10:26:48 +0300

    code updates

--- DotNet/DotNet.Test/DurableFunctionTest.cs
+++ DotNet/DotNet.Test/DurableFunctionTest.cs
@@ -6,6 +6,7 @@ using Moq;
 using System.Threading.Tasks;
 using Microsoft.Azure.WebJobs;
 using System.Net.Http;
+using Microsoft.Azure.WebJobs.Extensions.DurableTask;
 
 namespace DotNet.Test
 {
@@ -15,7 +16,7 @@ namespace DotNet.Test
         [TestMethod]
         public async Task Run_Orchectrator()
         {
-            var contextMock = new Mock<DurableOrchestrationContextBase>(); 
+            var contextMock = GetContextMock();
             contextMock.Setup(context => context.CallActivityAsync<string>("DurableFunctions_Hello", "Tokyo")).Returns(Task.FromResult<string>("Hello Tokyo!"));
             contextMock.Setup(context => context.CallActivityAsync<string>("DurableFunctions_Hello", "Seattle")).Returns(Task.FromResult<string>("Hello Seattle!"));
             contextMock.Setup(context => context.CallActivityAsync<string>("DurableFunctions_Hello", "London")).Returns(Task.FromResult<string>("Hello London!"));
@@ -25,33 +26,65 @@ namespace DotNet.Test
             Assert.AreEqual("Hello London!", result[2]);
         }
 
+#pragma warning disable CS0618 // Type or member is obsolete
+        private static Mock<DurableOrchestrationContextBase> GetContextMock()
+#pragma warning restore CS0618 // Type or member is obsolete
+        {
+#pragma warning disable CS0618 // Type or member is obsolete
+            return new Mock<DurableOrchestrationContextBase>();
+#pragma warning restore CS0618 // Type or member is obsolete
+        }
+
         [TestMethod]
+#pragma warning disable CS1998 // Async method lacks 'await' operators and will run synchronously
         public async Task Run_Hello_Activity()
+#pragma warning restore CS1998 // Async method lacks 'await' operators and will run synchronously
         {
             var result = DotNet.DurableFunctions.SayHello("Amsterdam", log);
             Assert.AreEqual("Hello Amsterdam!", result);
         }
 
         [TestMethod]
+#pragma warning disable CS1998 // Async method lacks 'await' operators and will run synchronously
         public async Task Run_Orchectrator_Client()
+#pragma warning restore CS1998 // Async method lacks 'await' operators and will run synchronously
         {
+#pragma warning disable CS0618 // Type or member is obsolete
             var clientMock = new Mock<DurableOrchestrationClientBase>();
-            // https://github.com/Azure/azure-functions-durable-extension/blob/0345b369ffa1745c24ffbacfaf8a43fb62dd2572/src/WebJobs.Extensions.DurableTask/DurableOrchestrationClient.cs#L46
+#pragma warning restore CS0618 // Type or member is obsolete
+                              // https://github.com/Azure/azure-functions-durable-extension/blob/0345b369ffa1745c24ffbacfaf8a43fb62dd2572/src/WebJobs.Extensions.DurableTask/DurableOrchestrationClient.cs#L46
             var requestMock = new Mock<HttpRequestMessage>();
             var id = "8e503c5e-19de-40e1-932d-298c4263115b";
             clientMock.Setup(client => client.StartNewAsync("DurableFunctions", null)).Returns(Task.FromResult<string>(id));
             var request = requestMock.Object;
-            clientMock.Setup(client => client.CreateCheckStatusResponse(request, id));
-            var result = DotNet.DurableFunctions.HttpStart(request, clientMock.Object, log);
+            _ = clientMock.Setup(client => NewMethod(client, id, request));
+#pragma warning disable CS0612 // Type or member is obsolete
+            var result = DurableFunctions.HttpStart(request, clientMock.Object, log);
+#pragma warning restore CS0612 // Type or member is obsolete
             try
             {
 
                 clientMock.Verify(client => client.StartNewAsync("DurableFunctions", null));
-                clientMock.Verify(client => client.CreateCheckStatusResponse(request, id));
-            } catch (MockException ex)
+                clientMock.Verify(client => NewMethod1(client, id, request));
+            }
+            catch (MockException)
             {
                 Assert.Fail();
             }
+        }
+
+#pragma warning disable CS0618 // Type or member is obsolete
+        private static HttpResponseMessage NewMethod1(DurableOrchestrationClientBase client, string id, HttpRequestMessage request)
+#pragma warning restore CS0618 // Type or member is obsolete
+        {
+            return client.CreateCheckStatusResponse(request, id);
+        }
+
+#pragma warning disable CS0618 // Type or member is obsolete
+        private static HttpResponseMessage NewMethod(DurableOrchestrationClientBase client, string id, HttpRequestMessage request)
+#pragma warning restore CS0618 // Type or member is obsolete
+        {
+            return client.CreateCheckStatusResponse(request, id);
         }
     }
 }
--- DotNet/DotNet/DurableFunctions.cs
+++ DotNet/DotNet/DurableFunctions.cs
@@ -2,6 +2,7 @@ using System.Collections.Generic;
 using System.Net.Http;
 using System.Threading.Tasks;
 using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.DurableTask;
 using Microsoft.Azure.WebJobs.Extensions.Http;
 using Microsoft.Azure.WebJobs.Host;
 
@@ -11,30 +12,37 @@ namespace DotNet
     {
         [FunctionName("DurableFunctions")]
         public static async Task<List<string>> RunOrchestrator(
-            [OrchestrationTrigger] DurableOrchestrationContextBase context)
+            [OrchestrationTrigger] IDurableOrchestrationContext context)
         {
-            var outputs = new List<string>();
+            var outputs = new List<string>
+            {
 
-            // Replace "hello" with the name of your Durable Activity Function.
-            outputs.Add(await context.CallActivityAsync<string>("DurableFunctions_Hello", "Tokyo"));
-            outputs.Add(await context.CallActivityAsync<string>("DurableFunctions_Hello", "Seattle"));
-            outputs.Add(await context.CallActivityAsync<string>("DurableFunctions_Hello", "London"));
+                // Replace "hello" with the name of your Durable Activity Function.
+                await context.CallActivityAsync<string>("DurableFunctions_Hello", "Tokyo"),
+                await context.CallActivityAsync<string>("DurableFunctions_Hello", "Seattle"),
+                await context.CallActivityAsync<string>("DurableFunctions_Hello", "London")
+            };
 
             // returns ["Hello Tokyo!", "Hello Seattle!", "Hello London!"]
             return outputs;
         }
 
         [FunctionName("DurableFunctions_Hello")]
+#pragma warning disable CS0618 // Type or member is obsolete
         public static string SayHello([ActivityTrigger] string name, TraceWriter log)
+#pragma warning restore CS0618 // Type or member is obsolete
         {
             log.Info($"Saying hello to {name}.");
             return $"Hello {name}!";
         }
 
         [FunctionName("DurableFunctions_HttpStart")]
+        [System.Obsolete]
         public static async Task<HttpResponseMessage> HttpStart(
             [HttpTrigger(AuthorizationLevel.Anonymous, "get", "post")]HttpRequestMessage req,
+#pragma warning disable CS0618 // Type or member is obsolete
             [OrchestrationClient]DurableOrchestrationClientBase starter,
+#pragma warning restore CS0618 // Type or member is obsolete
             TraceWriter log)
         {
             // Function input comes from the request content.
